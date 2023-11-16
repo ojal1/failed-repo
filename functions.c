c77c6117165954faefb2a6365e6b600abcb5767c
juancollazo5 <juancollazo577@outlook.com>
2023-11-16 22:52:03 +0000

    functions d, i, remake

--- functions.c
+++ functions.c
@@ -1,19 +1,76 @@
 #include "main.h"
 
 /**
- * _printint - print integers.
- * @values:  passed values.
- * Return:  passed values.
+ * _printint_d - print a signed integer.
+ * @args: parameters.
+ * Return: Count.
  */
-int _printint(va_list values)
-{
-	int num = va_arg(values, int);
-	int count = 0;
-	char buffer[20];
 
-	count += sprintf(buffer, "%d", num);
-	write(1, buffer, count);
+int _printint_d(va_list args)
+{
+	int count = 0;
+	char num_str[12];
+	int len = 0, i, num = va_arg(args, int);
+
+	if (num == 0)
+	{
+		write(1, "0", 1);
+		count++;
+	}
+
+	if (num < 0)
+	{
+		write(1, "-", 1);
+		count++;
+		num = -num;
+	}
+
+	while (num != 0)
+	{
+		num_str[len++] = num % 10 + '0';
+		num /= 10;
+	}
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		write(1, &num_str[i], 1);
+		count++;
+	}
+
 	return (count);
 }
 
+/**
+ * print_int_i - print a unsigned integer.
+ * @args: Parameters.
+ * Return: Count.
+ */
 
+int _printint_i(va_list args)
+{
+	int count = 0;
+	char num_str[12];
+	int len = 0, i, num = va_arg(args, int);
+
+	if (num == 0)
+	{
+		write(1, "0", 1);
+		count++;
+	}
+	else
+	{
+		while (num != 0)
+		{
+			num_str[len++] = num % 10 + '0';
+			num /= 10;
+		}
+
+		for (i = len - 1; i >= 0; i--)
+		{
+			write(1, &num_str[i], 1);
+			count++;
+		}
+	}
+
+	return (count);
+}
