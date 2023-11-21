87b6a32b86ed4871910f945c5078a68d368ff8df
juancollazo5 <juancollazo577@outlook.com>
2023-11-21 05:34:28 +0000

    print int update

--- functions.c
+++ functions.c
@@ -1,22 +1,33 @@
 #include "main.h"
-
 /**
- * print_int - print integer
- * @num: ... 
+ * _print_int - print numbers
+ * @args: Arguments
+ * Return: count
  */
+int _print_int(va_list args)
+{
+	long int n = va_arg(args, int), i = 1;
+	int count = 0;
 
-void print_int(int num) {
-    char buffer[12];
-    int i = 0;
-    while (num != 0) {
-        buffer[i++] = '0' + num % 10;
-        num /= 10;
-    }
-    if (i == 0) {
-        buffer[i++] = '0';
-    }
-
-    while (i > 0) {
-        write(STDOUT_FILENO, &buffer[--i], 1);
-    }
+	if (n == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+		count++;
+	}
+	for (i = 10; i <= n; i *= 10)
+	{
+	}
+	i /= 10;
+	for (; i > 0; i /= 10)
+	{
+		_putchar(((n / i) % 10) + '0');
+		count++;
+	}
+	return (count);
 }
