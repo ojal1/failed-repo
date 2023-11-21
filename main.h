ce9a349b0cfbf5346f06cc49abf79ea0c541edd8
osvaldo antompietri <antompietriosvaldo@gmail.com>
2023-11-21 10:48:28 -0400

    Update

--- main.h
+++ main.h
@@ -4,10 +4,13 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <stddef.h>
 
 /*Function Prototypes*/
 int _printf(const char *format, ...);
+int _printint_d(va_list *args);
+int _printint_i(va_list *args);
+int _printint_s(va_list *args);
+int _printint_c(va_list *args);
 int _putchar(char c);
-int _printint_d(va_list args);
-int _printint_i(va_list args);
 #endif
