92fd99fbb2a9be77a397c6c40ad6c2849bb275ab
juancollazo5 <juancollazo577@outlook.com>
2023-11-21 05:54:37 +0000

    print int update

--- functions.c
+++ functions.c
@@ -1,33 +1,35 @@
 #include "main.h"
-/**
- * _print_int - print numbers
- * @args: Arguments
- * Return: count
- */
-int _print_int(va_list args)
-{
-	long int n = va_arg(args, int), i = 1;
-	int count = 0;
 
-	if (n == 0)
-	{
-		_putchar('0');
-		return (1);
-	}
-	if (n < 0)
-	{
-		_putchar('-');
-		n = n * -1;
-		count++;
-	}
-	for (i = 10; i <= n; i *= 10)
-	{
-	}
-	i /= 10;
-	for (; i > 0; i /= 10)
-	{
-		_putchar(((n / i) % 10) + '0');
-		count++;
-	}
-	return (count);
+/**
+ * _print_int - Print numbers based on the specifier.
+ * @specifier: character ('d' or 'i').
+ * @args: Variable argument
+ * @count: Pointer to the count of printed characters.
+ */
+void _print_int(char specifier, va_list args, int *count)
+{
+    if (specifier == 'd' || specifier == 'i')
+    {
+        int number = va_arg(args, int);
+        int divider = 1;
+
+        if (number < 0)
+        {
+            write(1, "-", 1);
+            (*count)++;
+            number = -number;
+        }
+
+        while (number / divider > 9)
+            divider *= 10;
+
+        while (divider != 0)
+        {
+            char digit = '0' + number / divider;
+            write(1, &digit, 1);
+            (*count)++;
+            number %= divider;
+            divider /= 10;
+        }
+    }
 }
