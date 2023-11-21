0210036c2f1cd9afc6847894e5638616dc635b88
juancollazo5 <juancollazo577@outlook.com>
2023-11-21 04:40:47 +0000

    ignore

--- _printf.c
+++ _printf.c
@@ -1,71 +1,62 @@
 #include "main.h"
-#include <stdarg.h>
-#include <unistd.h>
-
 /**
- * _printf - Recreates the printf function
- * @format: A character string
- * Return: Number of characters printed
+ *_printf - recreating the printf function
+ *@format: is a character string
+ *Return: 0
  */
 int _printf(const char *format, ...)
 {
-    int i = 0, j = 0;
-    char *ptr;
-    int count = 0;
+	int i = 0, j = 0;
+	char *ptr;
+	int count = 0;
 
-    va_list args;
-    va_start(args, format);
+	va_list(args);
+	va_start(args, format);
 
-    while (format != NULL && format[i])
-    {
-        if (format[i] == '%')
-        {
-            i++;
+	while (format != NULL && format[i])
+	{
+		if (format[i] == '%')
+		{
+			format++;
 
-            switch (format[i])
-            {
-            case '%':
-                write(1, &format[i], 1);
-                count++;
-                break;
+			switch (format[i])
+			{
+				case '%': /* case of a % */
+					write(1, &format[i], 1);
+					break;
 
-            case 's':
-                ptr = va_arg(args, char *);
-                if (ptr == NULL)
-                {
-                    write(1, "(NULL)", 6);
-                    count += 6;
-                }
-                else
-                {
-                    while (ptr[j] != '\0')
-                    {
-                        write(1, &ptr[j], 1);
-                        j++;
-                        count++;
-                    }
-                }
-                break;
+				case 's': /* case of a string */
+					ptr = va_arg(args, char *);
+					if (ptr == NULL)
+					{
+						write(1, "(NILL)", 6);
+						return (6);
+					}
+					while (ptr[j] != '\0')
+					{	
+						write(1, &ptr[j], 1);
+						j++;
+						count++;
+					}
+					break;
 
-            case 'c':
-                // Use int for 'c' type and no need to check for NULL
-                write(1, &va_arg(args, int), 1);
-                count++;
-                break;
-
-            default:
-                write(1, "%", 1);
-                write(1, &format[i], 1);
-                count += 2;
-            }
-        }
-        else
-        {
-            write(1, &format[i], 1);
-            count++;
-        }
-        i++;
-    }
-    va_end(args);
-    return count;
+				case 'c': /* case of single character */
+					ptr = va_arg(args, char *);
+					if (ptr == NULL)
+					{
+						write(1, "(NILL)", 6);
+					}
+						write(1, &ptr, 1);
+					break;
+				default:
+					write(1, "%", 1);
+					write(1, &format[i], 1);
+			}
+		}
+		else
+			write(1, &format[i], 1);
+		i++;
+	}
+	va_end(args);
+	return (0);
 }
