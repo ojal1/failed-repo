57263478a1d50a2d79870573b60a516475b46136
osvaldo antompietri <antompietriosvaldo@gmail.com>
2023-11-21 10:03:11 -0400

    Update

--- _printf.c
+++ _printf.c
@@ -21,36 +21,36 @@ int _printf(const char *format, ...)
 
 			switch (format[i])
 			{
-				case '%': /* case of a % */
-					write(1, &format[i], 1);
-					break;
+			case '%': /* case of a % */
+				write(1, &format[i], 1);
+				break;
 
-				case 's': /* case of a string */
-					ptr = va_arg(args, char *);
-					if (ptr == NULL)
-					{
-						write(1, "(null)", 6);
-					}
-					while (ptr[j] != '\0')
-					{
-						write(1, &ptr[j], 1);
-						j++;
-						count++;
-					}
-					break;
+			case 's': /* case of a string */
+				ptr = va_arg(args, char *);
+				if (ptr == NULL)
+				{
+					write(1, "(null)", 6);
+				}
+				while (ptr[j] != '\0')
+				{
+					write(1, &ptr[j], 1);
+					j++;
+					count++;
+				}
+				break;
 
-				case 'c': /* case of single character */
-					ptr = va_arg(args, char *);
-					if (ptr == NULL)
-					{
-						write(1, "(null)", 6);
-						return (6);
-					}
-					write(1, &ptr, 1);
-					break;
-				default:
-					write(1, "%", 1);
-					write(1, &format[i], 1);
+			case 'c': /* case of single character */
+				ptr = va_arg(args, char *);
+				if (ptr == NULL)
+				{
+					write(1, "(null)", 6);
+					return (6);
+				}
+				write(1, &ptr, 1);
+				break;
+			default:
+				write(1, "%", 1);
+				write(1, &format[i], 1);
 			}
 		}
 		else
