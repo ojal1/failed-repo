8a959f7d109609d27d6f4703a3509a20f98e6dd3
juancollazo5 <juancollazo577@outlook.com>
2023-11-21 04:29:29 +0000

    d, i function

--- functions.c
+++ functions.c
@@ -1,78 +1,22 @@
 #include "main.h"
-#include <stdarg.h>
-#include <stdio.h>
 
 /**
- * _printint_d - print a signed integer.
- * @args: parameters.
- * Return: Count.
+ * print_int - print integer
+ * @num: ... 
  */
 
-int _printint_d(va_list args)
-{
-	int count = 0;
-	char num_str[12];
-	int len = 0, i, num = va_arg(args, int);
+void print_int(int num) {
+    char buffer[12];
+    int i = 0;
+    while (num != 0) {
+        buffer[i++] = '0' + num % 10;
+        num /= 10;
+    }
+    if (i == 0) {
+        buffer[i++] = '0';
+    }
 
-	if (num == 0)
-	{
-		write(1, "0", 1);
-		count++;
-	}
-
-	if (num < 0)
-	{
-		write(1, "-", 1);
-		count++;
-		num = -num;
-	}
-
-	while (num != 0)
-	{
-		num_str[len++] = num % 10 + '0';
-		num /= 10;
-	}
-
-	for (i = len - 1; i >= 0; i--)
-	{
-		write(1, &num_str[i], 1);
-		count++;
-	}
-
-	return (count);
-}
-
-/**
- * _printint_i - print a unsigned integer.
- * @args: Parameters.
- * Return: Count.
- */
-
-int _printint_i(va_list args)
-{
-	int count = 0;
-	char num_str[12];
-	int len = 0, i, num = va_arg(args, int);
-
-	if (num == 0)
-	{
-		write(1, "0", 1);
-		count++;
-	}
-	else
-	{
-		while (num != 0)
-		{
-			num_str[len++] = num % 10 + '0';
-			num /= 10;
-		}
-
-		for (i = len - 1; i >= 0; i--)
-		{
-			write(1, &num_str[i], 1);
-			count++;
-		}
-	}
-
-	return (count);
+    while (i > 0) {
+        write(STDOUT_FILENO, &buffer[--i], 1);
+    }
 }
