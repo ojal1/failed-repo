fa3cb6971296a152201c8c8828df8efac6006f5c
juancollazo5 <juancollazo577@outlook.com>
2023-11-21 15:07:25 +0000

    add function

--- functions.c
+++ functions.c
@@ -1,22 +1,25 @@
 #include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
 
 /**
- * print_int - print integer
- * @num: ... 
- */
+* _print_int - print integer
+* @value: the integer to print
+*/
+void _print_int(int value)
+{
+	char digit;
 
-void print_int(int num) {
-    char buffer[12];
-    int i = 0;
-    while (num != 0) {
-        buffer[i++] = '0' + num % 10;
-        num /= 10;
-    }
-    if (i == 0) {
-        buffer[i++] = '0';
-    }
+	if (value < 0) 
+{
+	write(1, "-", 1);
+	value = -value;
+}
 
-    while (i > 0) {
-        write(STDOUT_FILENO, &buffer[--i], 1);
-    }
+	if (value >= 10) 
+{
+	_print_int(value / 10);
+}
+	digit = value % 10 + '0';
+	write(1, &digit, 1);
 }
